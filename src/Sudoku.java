dfe41e8ab84eeea678c9f68be901dcaac8e40238
Axnou <68817935+Axnou@users.noreply.github.com>
2020-07-26 20:08:08 +0300

    Add files via upload

---
+++ src/Sudoku.java
@@ -0,0 +1,88 @@
+
+public class Sudoku {
+    public static void main(String[] args) {
+        int[][] sudoku = { { 0, 9, 0, 0, 5, 7, 0, 0, 0 }, { 2, 0, 0, 0, 0, 0, 0, 0, 5 }, { 0, 0, 0, 0, 0, 4, 0, 0, 6 },
+                { 0, 6, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 5, 0, 6, 3, 4, 0, 0 }, { 3, 0, 0, 9, 0, 8, 0, 0, 0 },
+                { 0, 3, 0, 0, 7, 0, 0, 8, 0 }, { 0, 2, 7, 0, 0, 9, 0, 0, 0 }, { 0, 4, 0, 8, 0, 1, 0, 5, 0 } };
+
+
+        solver(sudoku);
+        sudokuPrint(sudoku);
+    }
+
+    //Ratkaisija joka kayttaa backtracking algoritmia. Jotta algoritmi toimii, taytyy ratkaisijan palauttaa totuusarvo.
+    public static boolean solver(int[][] sudokuToSolve) {
+        Cordinate empty = findEmpty(sudokuToSolve);
+        if (empty == null) {
+            return true;
+        }
+
+        int row = empty.getY();
+        int column = empty.getX();
+
+        for (int i = 1; i <= 9; i++) {
+            sudokuToSolve[row][column] = i;
+            if (acceptable(sudokuToSolve, row, column, i)) {
+                if (solver(sudokuToSolve)) {
+                    return true;
+                }
+                sudokuToSolve[row][column] = 0;
+            } else {
+                sudokuToSolve[row][column] = 0;
+            }
+        }
+        return false;
+    }
+
+    // Katsoo onko taulukkoon laitettava arvo hyvaksyttava
+    public static boolean acceptable(int[][] board, int r, int c, int num) {
+
+        // varmistaa onko rivilla samaa numeroa
+        for (int i = 0; i < 9; i++) {
+            if (board[i][c] == num && i != r) {
+                return false;
+            }
+        }
+
+        // varmistaa onko sarakkeessa samaa numeroa
+        for (int i = 0; i < 9; i++) {
+            if (board[r][i] == num && i != c) {
+                return false;
+            }
+        }
+
+        // varmistaa onko sudokun 3x3 laatikossa samaa lukua
+        for (int i = 0; i < 3; i++) {
+            int ybox = (r / 3) * 3 + i;
+            for (int a = 0; a < 3; a++) {
+                int xbox = (c / 3) * 3 + a;
+                if (board[ybox][xbox] == num && r != ybox && c != xbox) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Etsii tyhjän ruudun (ruudun jossa on 0)
+    public static Cordinate findEmpty(int[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int a = 0; a < 9; a++) {
+                if (board[i][a] == 0) {
+                    return new Cordinate(i, a);
+                }
+            }
+        }
+        return null;
+    }
+
+    // tulostaa sudokun
+    public static void sudokuPrint(int[][] printSudoku) {
+        for (int row = 0; row < 9; row++) {
+            for (int column = 0; column < 9; column++) {
+                System.out.print(printSudoku[row][column] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
